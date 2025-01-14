d679728510577545ace19459f2ccb00fcc8859c1
Vikram Kumar <121078328+Vikramveera-28@users.noreply.github.com>
2025-01-14 17:10:01 +0530

    Update apiUrl.js

--- src/Api/apiUrl.js
+++ src/Api/apiUrl.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    // baseURL : 'https://json-restaurant.onrender.com'
-    baseURL : 'http://localhost:3500'
+    baseURL : 'https://json-restaurant.onrender.com'
+    // baseURL : 'http://localhost:3500'
 })
